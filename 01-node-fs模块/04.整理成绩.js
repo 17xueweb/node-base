ebfd064ea0abe8a6e1318a6dbd7320d55a6cfacf
lcruan <1416802331@qq.com>
2024-01-23 23:21:32 +0800

    node 成绩整理案例

--- 01-node-fs模块/04.整理成绩.js
+++ 01-node-fs模块/04.整理成绩.js
@@ -7,6 +7,27 @@ fs.readFile('./files/成绩.txt', 'utf8', function(err, dataStr) {
     if (err) {
         return console.log('文件读取失败！' + err.message);
     }
+    //   console.log('文件读取成功！' + dataStr);
 
-    console.log('文件读取成功！' + dataStr);
+    // 4.1 先把成绩的数据，按照空格进行分割
+    const arrOld = dataStr.split(' ')
+    // 4.2 循环分割后的数组，对每一项数据，进行字符串的替换操作
+    const arrNew = []
+    arrOld.forEach(item => {
+        arrNew.push(item.replace('=', '：'))
+    })
+
+    // 4.3 把新数组的每一项，进行合并，得到第一个新的字符串
+    const newStr = arrNew.join('\r\n')
+
+    // 5. 调用 fs.writeFile() 方法，把处理完毕的成绩，写入到新的文件中
+    fs.writeFile('./files/成绩-ok.txt', newStr, function(err) {
+        if (err) {
+            return console.log('成绩写入失败！' + err.message);
+        }
+
+        console.log('成绩写入成功！');
+    })
+    
+
 })
