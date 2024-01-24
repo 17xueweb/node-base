f94b92aca8ba2c80307e9f7196c34e86f83c371a
lcruan <1416802331@qq.com>
2024-01-24 11:24:05 +0800

    node路径问题

---
+++ 01-node-fs模块/05.演示路径的问题.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+// 出现径拼接错误的问题，是因为提供了 ./ 或 ../开头的相对路径
+// 如果要解决这个问题，可以直接提供一个完整的文件存放路径就行
+// 代码运行时，会执行node命令时所处的目录，动态拼接出完整路径
+
+/* fs.readFile("./files/1.txt", 'utf8', function(err, dataStr) {
+    if (err) {
+        return console.log('文件读取失败！' + err.message);
+    }
+    console.log('文件读取成功！' + dataStr);
+}) */
+
+// 移植性差，不利于维护
+fs.readFile("/Users/learn/node/heima/code/node基础/node-base/01-node-fs模块/05.演示路径的问题.js", 'utf8', function(err, dataStr) {
+    if (err) {
+        return console.log('文件读取失败！' + err.message);
+    }
+    console.log('文件读取成功！' + dataStr);
+})
