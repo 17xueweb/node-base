6c7f64e9af7aab6281b8d5596518652ce56a6e90
lcruan <1416802331@qq.com>
2024-01-26 15:59:46 +0800

    开发npm包

--- 05-node-包/01.test.js
+++ 05-node-包/01.test.js
@@ -3,3 +3,10 @@ const itheima = require("./itheima-codeclear-tools")
 const dateStr = itheima.dateFormat(new Date())
 
 console.log(dateStr);
+
+const htmlStr = '<h1 title="abc">这是h1标签<span>123&nbsp;</span></h1>'
+const str = itheima.htmlEscape(htmlStr)
+console.log(str);
+
+const str2 = itheima.htmlUnEscape(str)
+console.log(str2);
--- 05-node-包/itheima-codeclear-tools/index.js
+++ 05-node-包/itheima-codeclear-tools/index.js
@@ -1,27 +1,12 @@
 // 这是包的入口文件
 
-// 定义格式化时间的函数
-function dateFormat(dateStr) {
-    const dt = new Date(dateStr)
+const date = require('./src/dateFormat')
+const escape = require('./src/htmlEscape')
 
-    const y = dt.getFullYear()
-    const m = padZero(dt.getMonth() + 1)
-    const d = padZero(dt.getDate())
-
-    const hh = padZero(dt.getHours())
-    const mm = padZero(dt.getMinutes())
-    const ss = padZero(dt.getSeconds())
-
-    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
-}
-
-// 定义一个补零的函数
-function padZero(n) {
-     return  n > 0 ? n : "0" + n
-}
 
 // 向外暴露 需要的成员
 module.exports = {
-    dateFormat
+    ...date,
+    ...escape
 }
  
